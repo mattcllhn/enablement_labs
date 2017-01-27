c826059cb80a28e5b3e455498b5435c0a4db0efd
Conor Budge <conorbudge555@gmail.com>
2017-01-27 10:58:40 -0600

    Working on JS for pager

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,11 @@
  */
 jQuery(document).ready(function ($) {
     //Function for smooth scroll to anchor on page
-    $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     $('html, body').animate({
                         scrollTop: target.offset().top
@@ -18,17 +18,48 @@ jQuery(document).ready(function ($) {
             }
         });
     });
-
-    $('#block-views-chapter-view-v2-block').on('click', '#chapter-back span', function(e) {
-        console.log("Clicked back");
-        $(".pager-previous a").click();
-        e.preventDefault();
+    
+     $('#chapter-back span').click(function(e) {
+        console.log("Clicked backward");
+        $(".pager-previous a")[0].click();
     });
-
-    $('#block-views-chapter-view-v2-block').on('click', '#chapter-forward span', function(e) {
+    
+    $('#chapter-forward span').click(function(e) {
         console.log("Clicked forward");
-        $(".pager-next a").click();
-        e.preventDefault();
+        $(".pager-next a")[0].click();
     });
+
+    var currentPage = $('.pager-current').index();
+
+    // var percentage =
+    $('.progress-bar').css('width', function () {
+
+    });
+
+    if( currentPage === 1) {
+        $('#chapter-back').addClass('hide-pagination');
+    } else if (currentPage === 12) {
+        $('#chapter-back').addClass('hide-pagination');
+    }
+    // } else {
+    //     $('#chapter-back').removeClass('hide-pagination');
+    // }
+
+    console.log("The text is: " + $(currentPage).text());
+
+    console.log("Current page is: " + currentPage);
+
+    
+//       $('#block-views-chapter-view-v2-block').on('click', '#chapter-back span', function(e) {
+//         console.log("Clicked back");
+//        $(".pager-previous a").click();
+//        //e.preventDefault();
+//    });
+//    
+//    $('#block-views-chapter-view-v2-block').on('click', '#chapter-forward span', function(e) {
+//        console.log("Clicked forward");
+//        $(".pager-next a").click();
+//        //e.preventDefault();
+//    });
 
 });
