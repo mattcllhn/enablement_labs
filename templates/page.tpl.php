82882bdfe2d672cd1d9b55d568a2a8d0cd386ad8
Luke Bauman <luke.bauman@concordusa.com>
2017-01-31 02:01:01 -0600

    fixed chapter header and side menu logic

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -27,7 +27,7 @@
         </div> 
     </div>
     <div class="secondary-offsides page-wrapper clearfix">
-        <div id="offside-wrapper" class="clearfix">
+        <div id="offside-wrapper" class="clearfix no-display">
             <?php if($page['offside']): ?>
                 <div id="offside">
                     <?php print render ($page['offside']); ?>
