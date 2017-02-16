6bb6b36744b3d163b7dc0fc4bfd3f0e845fd791f
Matt Callahan <mattcllhn@gmail.com>
2017-02-16 10:43:05 -0600

    changed templates to include tabs

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,56 +1,61 @@
 <?php
 ?>
 <div id="offside-navigation-wrapper" class="clearfix">
-    <?php if($page['navoffside']): ?>
-        <div id="offside-navigation">
-            <?php print render ($page['navoffside']); ?>
-        </div>
-    <?php endif; ?>
-</div> 
+  <?php if($page['navoffside']): ?>
+    <div id="offside-navigation">
+      <?php print render ($page['navoffside']); ?>
+    </div>
+  <?php endif; ?>
+</div>
 <div id="site">
-    <div id="navigation-wrapper" class="clearfix">
-        <div class="page-wrapper">
-        <?php if($page['navigation']): ?>
-            <div id="navigation">
-                <?php print render ($page['navigation']); ?>
-            </div>
+  <div id="navigation-wrapper" class="clearfix">
+    <div class="page-wrapper">
+      <?php if($page['navigation']): ?>
+        <div id="navigation">
+          <?php print render ($page['navigation']); ?>
+        </div>
+      <?php endif; ?>
+    </div>
+  </div>
+  <div class="page-wrapper">
+    <div id="content-wrapper" class="clearfix">
+      <?php if ($tabs): ?>
+        <div class="tabs">
+          <?php print render($tabs); ?>
+        </div>
+      <?php endif; ?>
+      <?php if($page['content']): ?>
+        <div id="content">
+          <?php print render ($page['content']); ?>
+        </div>
+      <?php endif; ?>
+    </div>
+  </div>
+  <div class="secondary-offsides page-wrapper clearfix">
+    <div id="offside-wrapper" class="clearfix">
+      <?php if($page['offside']): ?>
+        <div id="offside">
+          <?php print render ($page['offside']); ?>
+        </div>
+      <?php endif; ?>
+    </div>
+    <div class="page-wrapper">
+      <div id="secondary-content-wrapper" class="clearfix">
+        <?php if($page['content2']): ?>
+          <div id="content2">
+            <?php print render ($page['content2']); ?>
+          </div>
         <?php endif; ?>
+      </div>
+    </div>
+  </div>
+  <div class="page-wrapper">
+    <div id="footer-wrapper" class="clearfix">
+      <?php if($page['footer']): ?>
+        <div id="footer">
+          <?php print render ($page['footer']); ?>
         </div>
-    </div> 
-    <div class="page-wrapper">
-        <div id="content-wrapper" class="clearfix">
-            <?php if($page['content']): ?>
-                <div id="content">
-                    <?php print render ($page['content']); ?>
-                </div>
-            <?php endif; ?>
-        </div> 
-    </div>
-    <div class="secondary-offsides page-wrapper clearfix">
-        <div id="offside-wrapper" class="clearfix">
-            <?php if($page['offside']): ?>
-                <div id="offside">
-                    <?php print render ($page['offside']); ?>
-                </div>
-            <?php endif; ?>
-        </div> 
-        <div class="page-wrapper">
-            <div id="secondary-content-wrapper" class="clearfix">
-                <?php if($page['content2']): ?>
-                    <div id="content2">
-                        <?php print render ($page['content2']); ?>
-                    </div>
-                <?php endif; ?>
-            </div> 
-        </div>
-    </div>
-    <div class="page-wrapper">
-        <div id="footer-wrapper" class="clearfix">
-            <?php if($page['footer']): ?>
-                <div id="footer">
-                    <?php print render ($page['footer']); ?>
-                </div>
-            <?php endif; ?>
-        </div> <!-- end footer wrapper --> 
-    </div>
+      <?php endif; ?>
+    </div> <!-- end footer wrapper -->
+  </div>
 </div>
