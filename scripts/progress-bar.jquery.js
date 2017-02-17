97e0b8e4af6affafc0ffd08486873c10371591ef
Conor Budge <conorbudge555@gmail.com>
2017-02-17 15:55:18 -0600

    Updated progress bar

--- scripts/progress-bar.jquery.js
+++ scripts/progress-bar.jquery.js
@@ -2,9 +2,7 @@
  * Created by conor.budge on 2/15/2017.
  */
 jQuery(document).ready(function ($) {
-
-    var $numCompleted = parseInt($('.num-completed-chapters').text());
-    var $totalChapter = parseInt($('.total-chapters').text());
+    var $dataComplete = $('.progress-bar').data('percent-complete');
 
     if ($('.pager-current').length > 0) {
         progressBar();
@@ -12,10 +10,6 @@ jQuery(document).ready(function ($) {
 
 function progressBar() {
     var progressBar = $('.progress-bar');
-    progressBar.attr('style', "width: 0%");
-    console.log("First Value: " + $numCompleted + " Last Value: " + $totalChapter);
-    var percentage = (($numCompleted / $totalChapter) * 100);
-    console.log("Progress total " + percentage);
-    progressBar.css('width', percentage + '%');
+    progressBar.css('width', $dataComplete + "%");
 }
 });
