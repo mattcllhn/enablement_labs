8ea27b04022d50e4d9b2be2ccb118e03c1da7c69
Carter Gruss <carter.gruss@concordusa.com>
2017-02-20 15:30:59 -0600

    no message

--- template.php
+++ template.php
@@ -70,41 +70,4 @@ function enablement_views_mini_pager($vars) {
     );
   }
 }
-
-
-function enablement_pager_link($text, $page_new, $element, $parameters = array(), $attributes = array()) {
-  $page = isset($_GET['page']) ? $_GET['page'] : '';
-  if ($new_page = implode(',', pager_load_array($page_new[$element], $element, explode(',', $page)))) {
-    $parameters['page'] = $new_page;
-  }
-
-  // $query = array();
-  // if (count($parameters)) {
-  //   $query[] = drupal_query_string_encode($parameters, array());
-  // }
-  // $querystring = pager_get_querystring();
-  // if ($querystring != '') {
-  //   $query[] = $querystring;
-  // }
-
-  // Set each pager link title
-  if (!isset($attributes['title'])) {
-    static $titles = NULL;
-    if (!isset($titles)) {
-      $titles = array(
-        t('« first') => t('Go to first page'),
-        t('‹ previous') => t('Go to previous page'),
-        t('next ›') => t('Go to next page'),
-        t('last »') => t('Go to last page'),
-      );
-    }
-    if (isset($titles[$text])) {
-      $attributes['title'] = $titles[$text];
-    }
-    else if (is_numeric($text)) {
-      $attributes['title'] = t('Go to page @number', array('@number' => $text));
-    }
-  }
-  return l($text, $_GET['q'], array('attributes' => $attributes, 'html' => TRUE, 'query' => count($query) ? implode('&', $query) : NULL));
-}
 ?>
